4e8556a3c7d6d445dc9b2f4b6d93d922066d3b4e
ankitnayak1102 <105808855+ankitnayak1102@users.noreply.github.com>
2024-04-06 19:55:40 +0000

    container config done

---
+++ 01_basics/02 datatypes.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// alert( 3 + 3 ) we're using node js not browser
+
+console.log( 3 + 3)
+
+console.log("Ankit")
+
+let name = " Ankit"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+//string => ""
+//boolean => True/False
+//null => standalone value
+// undefined =>
+//symbol  => unique
+
+
+//object
+
+console.log(typeof null); // object
+console.log(typeof undefined); //object
+
+
+
