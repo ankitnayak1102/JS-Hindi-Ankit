89da1911f175f0e948b1778f827da80837091abf
ankitnayak1102 <105808855+ankitnayak1102@users.noreply.github.com>
2024-04-06 21:07:29 +0000

    container config done

---
+++ 01_basics/03 conversionoperator.js
@@ -0,0 +1,25 @@
+let score = 33
+
+console.log( typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+
+let isLoggedIn = 1
+let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);
+
+// 1 => true; 0=> false
+// "" => false
+//"ankit" => true
+
+let someNumber = 33
+let stringNumber = string(someNumber)
+console.log(typeof stringNumber);
