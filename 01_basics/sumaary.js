47e274b32f7a3e3269b60c554614dcc7bb7c18ef
ankitnayak1102 <105808855+ankitnayak1102@users.noreply.github.com>
2024-04-14 18:28:17 +0000

    hehe

--- 01_basics/sumaary.js
+++ 01_basics/sumaary.js
@@ -1,1 +1,7 @@
-console.log( " backend hi sabse badhiya hai yehi krlete hai ")
+// console.log( " backend hi sabse badhiya hai yehi krlete hai ")
+// console.log( " strings ")
+
+ Carname = "Supra";
+ supra = "supercar";
+
+ console.log("supercar");
